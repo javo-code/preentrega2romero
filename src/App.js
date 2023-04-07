3efecdae1a3de347d7fd17c6fc5a156036f8b64d
javier romero <romerofj26@gmail.com>
2023-04-07 18:41:31 -0300

    modifico texto para corroborar subida exitosa a ghpages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          texto prueba
+          texto prueba - subido a gh-pages
         </p>
         <a
           className="App-link"
